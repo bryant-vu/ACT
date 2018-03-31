da26ecd3c199b3a34df35c81e0f64985bda104b5
GTA <grant.aguinaldo@gmail.com>
2018-03-30 20:26:13 -0700

    heroku deployment -gta

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,45 @@
-var dataDropdown = d3.select('#questionDropdown')
 var questionEndPoint = '/api/v1/question_list'
-plotly.d3.json(questionEndPoint, function(error, response){
-    if error return console.warn(error):
+Plotly.d3.json(questionEndPoint, function(error, response) {
+    if (error) return console.warn(error);
 
-        for (var i = 0; i < response.length; i++){
-            console.log(response[i])
-            dataDropdown
-                .append('option')
-                .attr('value', response[i])
-                .text(response[i])
-        }
-})
+    for (var i = 0; i < response.length; i++){
+        d3.select("#questionDropdown")
+              .append('option')
+              .attr('value', response[i])
+              .text(response[i])
+    }
+
+});
+
+function getQuestionData() {
+
+        sampleValue = document.getElementById("questionDropdown").value;
+
+        document.getElementById("question").innerHTML = ""
+        document.getElementById("questionAnswers").innerHTML = ""
+
+        var endPointQuestionData = '/api/v1/questions/' + sampleValue
+        Plotly.d3.json(endPointQuestionData, function(error, response) {
+
+            if (error) return console.warn(error);
+
+            appendInnerHMTL(response)
+        });
+};
+
+function appendInnerHMTL(response) {
+
+    d3.select("#question")
+    .append('p')
+    .text(response[0]['Question'])
+
+    d3.select("#questionAnswers")
+    .append('li')
+    .text(response[0]['Answer_1'])
+    .append('li')
+    .text(response[0]['Answer_2'])
+    .append('li')
+    .text(response[0]['Answer_3'])
+    .append('li')
+    .text(response[0]['Answer_5'])
+};
