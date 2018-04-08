8de8b7f18bb75e928af38909600a9d4900bec0fd
bvu <bryantvu91@gmail.com>
2018-04-08 00:44:15 -0700

    new db

--- api.py
+++ api.py
@@ -9,35 +9,33 @@ Base.metadata.reflect(engine)
 
 
 class Questions(Base):
-  __table__ = Base.metadata.tables['circle']
+    __table__ = Base.metadata.tables['circle']
 
 
 session = scoped_session(sessionmaker(bind=engine))
 
 
 def question_list():
-  questionQuery = session.query(Questions.test_date)
+    questionQuery = session.query(Questions.test_date)
 
-  questionList = [each for (each,) in questionQuery]
+    questionList = [each for (each,) in questionQuery]
 
-  return questionList
+    return questionList
 
 
 def questions(input_data):
 
-  questionReturn = session.query(Questions.statement,
-                                 Questions.test_date,
-                                 Questions.choice_a,
-                                 Questions.choice_b,
-                                 Questions.choice_c,
-                                 Questions.choice_d).filter(Questions.test_date == input_data)
+    questionReturn = session.query(Questions.statement,
+                                   Questions.choice_a,
+                                   Questions.choice_b,
+                                   Questions.choice_c,
+                                   Questions.choice_d).filter(Questions.test_date == input_data)
 
-  dataDict = [{'Question': each[0],
-               'Test_Date': each[1],
-               'Answer_1': each[2],
-               'Answer_2': each[3],
-               'Answer_3': each[4],
-               'Answer_5': each[5]
-               } for each in questionReturn]
+    dataDict = [{'Question': each[0],
+                 'Answer_1': each[1],
+                 'Answer_2': each[2],
+                 'Answer_3': each[3],
+                 'Answer_5': each[4]
+                 } for each in questionReturn]
 
-  return dataDict
+    return dataDict
